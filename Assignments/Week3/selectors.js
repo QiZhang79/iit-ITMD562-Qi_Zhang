68210b687c6cf8be6f3babfc5c9817f0e49dcf19
Qi Zhang <zhangqitydx@gmail.com>
2017-09-12 00:13:52 -0500

    Add Loop

--- Assignments/Week3/selectors.js
+++ Assignments/Week3/selectors.js
@@ -1,36 +1,22 @@
+
 var main =function(){
 	'use strict;'
 
-	$('.relevant p:nth-child(1)').hide();
-	$('.relevant p:nth-child(1)').css('color','red');
-	$('.relevant p:nth-child(1)').fadeIn(2000);
+	var colors = ['red','orange','yellow','green','grey','blue','purple'];
 
-	$('.relevant p:nth-child(2)').hide();
-	$('.relevant p:nth-child(2)').css('color','orange');
-	$('.relevant p:nth-child(2)').delay(500).fadeIn(2000);
+	$('.relevant p').hide();
 
-	$('.relevant p:nth-child(3)').hide();
-	$('.relevant p:nth-child(3)').css('color','yellow');
-	$('.relevant p:nth-child(3)').delay(1000).fadeIn(2000);
+	for (i = 0; i < 7; i++) {
+		var num = $(this).find('.relevant p:nth-child('+(i+1)+')');
 
-	$('.relevant p:nth-child(4)').hide();
-	$('.relevant p:nth-child(4)').css('color','green');
-	$('.relevant p:nth-child(4)').delay(1500).fadeIn(2000);
-
-	$('.relevant p:nth-child(5)').hide();
-	$('.relevant p:nth-child(5)').css('color','grey');
-	$('.relevant p:nth-child(5)').delay(2000).fadeIn(2000);
-
-	$('.relevant p:nth-child(6)').hide();
-	$('.relevant p:nth-child(6)').css('color','blue');
-	$('.relevant p:nth-child(6)').delay(2500).fadeIn(2000);
-
-	$('.relevant p:nth-child(7)').hide();
-	$('.relevant p:nth-child(7)').css('color','purple');
-	$('.relevant p:nth-child(7)').delay(3000).fadeIn(2000);
+		num.css('color', colors[i]);
+		num.delay(i*500).fadeIn(2000);
+	}
 };
+
 
 $(document).ready(main);
 
 
-
+/* Tips:
+p:nth-child(i+1) is wrong. 
